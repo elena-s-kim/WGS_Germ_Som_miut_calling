6cb5672700bdc4d087c0f37f62f908202d013490
Elena S Kim <muripabi@gmail.com>
2025-03-05 14:18:15 -0500

    create_oncoplot.R

--- create_oncoplot.R
+++ create_oncoplot.R
@@ -5,10 +5,10 @@ if (!requireNamespace("maftools", quietly = TRUE))
 
 library(maftools)
 
-# Load the detailed MAF file
+# Load the detailed (or simplified) MAF file
 maf_file <- "/Users/elena_s_kim/Documents/Oncoprint_06122024/combined_variants_dedup.maf"
 maf <- read.maf(maf = maf_file, verbose = FALSE)
-
+'''
 # Check if MAF contains data
 if (nrow(maf@data) == 0) {
   stop("No non-synonymous mutations found. Check the data extraction process.")
@@ -16,6 +16,9 @@ if (nrow(maf@data) == 0) {
 
 # Print the structure of the MAF data for debugging
 str(maf@data)
+'''
+# Generate the oncoplot directly or see next
+oncoplot(maf = maf, draw_titv = TRUE, top = 20)
 
 # Generate the oncoplot and save it as a PDF
 output_dir <- "/Users/elena_s_kim/Documents/Oncoprint_06122024/"
@@ -24,3 +27,6 @@ output_file <- file.path(output_dir, "oncoprint.pdf")
 pdf(output_file, width = 12, height = 8)
 oncoplot(maf = maf, top = 20)
 dev.off()
+
+
+
